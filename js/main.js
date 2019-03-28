d3c4a4eaa48b0c00e91ac23dce4fd8c588d4c722
Geri Rosenberg <gbrosenberg@wisc.edu>
2019-03-28 16:16:03 -0400

    draft4

--- js/main.js
+++ js/main.js
@@ -5,6 +5,30 @@ window.onload = setMap();
 
 // set up choropleth map
 function setMap(){
+
+	// map frame dimensions
+	var width =500,
+	height = 460;
+
+	// create new svg container for the map
+	var map = d3.select("body")
+	.append("svg")
+	.attr("class", "map")
+	.attr("width", width)
+	.attr("height", height);
+
+	// create Albers equal area conic projection
+	var projection = d3.geoConicConformal()
+	.center([-0.08, 38.95])
+	.rotate([77, 0])
+	.parallels([38.3, 39.45])
+	.scale(30000)
+	.translate([width / 2, height / 2]);
+
+	// path generator
+	var path = d3.geoPath()
+		.projection(projection);
+
 	// use queue to parallelize asynchronous data loading
 	d3.queue()
 		// load attributes from csv
@@ -13,26 +37,52 @@ function setMap(){
 		.defer(d3.json, "data/allcounties.topojson")
 		// load choropleth spatial data
 		.defer(d3.json, "data/tracts.topojson")
-		// load county outlines
+		// // load county outlines
 		.defer(d3.json, "data/countyoutlines.topojson")
 		// load state outlines
 		.defer(d3.json, "data/stateoutlines.topojson")
 		.await(callback);
 
 	// add callback function
-	function callback(error, csvData, counties, tracts, countyOutlines, stateOutlines){
-		console.log(counties);
-		console.log(tracts);
-		console.log(countyOutlines);
-		console.log(stateOutlines);
-		
+	function callback(error, csvData, counties, dctracts, countyOutlines, stateOutlines){
+
+		// create graticule generator
+		var graticule = d3.geoGraticule()
+			// place graticule lines every 1deg lat and long
+			.step([1, 1]);
+
 		// translate topoJSON
 		var allCounties = topojson.feature(counties, counties.objects.dccounties),
-			allTracts = topojson.feature(tracts, tracts.objects.tracts).features,
-			linesCounties = topojson.feature(countyOutlines.objects.regioncounties),
+			allTracts = topojson.feature(dctracts, dctracts.objects.tracts).features,
+			linesCounties = topojson.feature(countyOutlines, countyOutlines.objects.countylines),
 			linesStates = topojson.feature(stateOutlines, stateOutlines.objects.states);
 
-		// examine results
-		console.log(allTracts);
+		// add counties to map background
+		var countiesbg = map.append("path")
+			.datum(allCounties)
+			.attr("class", "countiesbg")
+			.attr("d", path);
+
+		// add tracts to map
+		var censustracts = map.selectAll(".censustracts")
+			.data(allTracts)
+			.enter()
+			.append("path")
+			.attr("class", function(d){
+				return "censustracts" + d.properties.Id2;
+			})
+			.attr("d", path);
+
+		// add county lines to map
+		var countylines = map.append("path")
+			.datum(linesCounties)
+			.attr("class", "countylines")
+			.attr("d", path);
+
+		// add state lines to map
+		var statelines = map.append("path")
+			.datum(linesStates)
+			.attr("class", "statelines")
+			.attr("d", path);
 	};
 };
