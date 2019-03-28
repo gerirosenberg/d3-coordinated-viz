62a881587ed40ad687dc0f6c2ce05626d6c31fa2
Geri Rosenberg <gbrosenberg@wisc.edu>
2019-03-28 09:45:51 -0400

    draft2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 /* Javascript by Geri Rosenberg, 2019 */
+
+// begin script when window loads
+window.onload = setMap();
+
+// set up choropleth map
+function setMap(){
+	// use queue to parallelize asynchronous data loading
+	d3.queue()
+		// load attributes from csv
+		.defer(d3.csv, "data/traveltime.csv")
+		// load background spatial data
+		.defer(d3.json, "data/all-counties.topojson")
+		// load choropleth spatial data
+		.defer(d3.json, "data/all-tracts.topojson")
+		// load county outlines
+		.defer(d3.json, "data/county-outlines.topojson")
+		// load state outlines
+		.defer(d3.json, "data/state-outlines.topojson")
+		.await(callback);
+
+	// add callback function
+	function callback(error, csvData, counties, tracts, countyOutlines, stateOutlines){
+		console.log(error);
+		console.log(csvData);
+		console.log(counties);
+		console.log(tracts);
+		console.log(countyOutlines);
+		console.log(stateOutlines);
+	};
+};
