ee0e78992ec3ec8ed90cf14436cf4375107bce25
Geri Rosenberg <gbrosenberg@wisc.edu>
2019-03-28 14:48:27 -0400

    draft3

--- js/main.js
+++ js/main.js
@@ -10,22 +10,29 @@ function setMap(){
 		// load attributes from csv
 		.defer(d3.csv, "data/traveltime.csv")
 		// load background spatial data
-		.defer(d3.json, "data/all-counties.topojson")
+		.defer(d3.json, "data/allcounties.topojson")
 		// load choropleth spatial data
-		.defer(d3.json, "data/all-tracts.topojson")
+		.defer(d3.json, "data/tracts.topojson")
 		// load county outlines
-		.defer(d3.json, "data/county-outlines.topojson")
+		.defer(d3.json, "data/countyoutlines.topojson")
 		// load state outlines
-		.defer(d3.json, "data/state-outlines.topojson")
+		.defer(d3.json, "data/stateoutlines.topojson")
 		.await(callback);
 
 	// add callback function
 	function callback(error, csvData, counties, tracts, countyOutlines, stateOutlines){
-		console.log(error);
-		console.log(csvData);
 		console.log(counties);
 		console.log(tracts);
 		console.log(countyOutlines);
 		console.log(stateOutlines);
+		
+		// translate topoJSON
+		var allCounties = topojson.feature(counties, counties.objects.dccounties),
+			allTracts = topojson.feature(tracts, tracts.objects.tracts).features,
+			linesCounties = topojson.feature(countyOutlines.objects.regioncounties),
+			linesStates = topojson.feature(stateOutlines, stateOutlines.objects.states);
+
+		// examine results
+		console.log(allTracts);
 	};
 };
